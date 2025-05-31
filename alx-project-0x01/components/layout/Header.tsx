3e6da0ae776cb51e79ae9c3b34159fe6603a1889
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-05-31 23:38:53 +0200

    The Daily content

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,9 +1,25 @@
-const Footer: React.FC = () => {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-7xl font-thin">Footer Page</h1>
+import Link from 'next/link'; 
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+            <Link href="/">Daily Contents</Link>
+        </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    )
-  }
-  
-   export default Footer;
+    </header>
+  );
+}
+
+export default Header;
