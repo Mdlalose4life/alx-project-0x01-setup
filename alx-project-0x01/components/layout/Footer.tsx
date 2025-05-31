76ae433685ec8b533748a8ca4146f7bd7209af04
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-05-31 23:27:34 +0200

    Adding the pages and content

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-7xl font-thin">Header Page</h1>
+      </div>
+    )
+  }
+  
+   export default Header;
