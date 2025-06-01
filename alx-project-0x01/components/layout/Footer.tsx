5aab8441bb267af82145cec125a28587c4ab8530
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-06-01 16:17:02 +0200

    Restart

--- alx-project-0x01/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-7xl font-thin">Header Page</h1>
-      </div>
-    )
-  }
-  
-   export default Header;
